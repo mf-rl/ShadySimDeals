8ef083e0b4916508f7668c32690e4b81de12f205
mf-rl <mf.ramirezl@gmail.com>
2026-08-05 14:18:18 -0500

    chore: trace rabbit hole interaction lifecycle

--- src/shady_sim_deals/rabbit_hole_interaction.py
+++ src/shady_sim_deals/rabbit_hole_interaction.py
@@ -1,9 +1,32 @@
 from interactions.base.super_interaction import SuperInteraction
 from interactions.rabbit_hole import HideSimLiability
 
+from .logging import ModLogger
+
+
+LOGGER = ModLogger()
+
+
+class _LoggedHideSimLiability(HideSimLiability):
+    def on_run(self):
+        LOGGER.log("rabbit_hole_interaction_running")
+        return super().on_run()
+
+    def release(self):
+        LOGGER.log(
+            "rabbit_hole_interaction_releasing",
+            finishing_naturally=bool(
+                getattr(self._interaction, "is_finishing_naturally", False)
+            ),
+            finishing_type=str(
+                getattr(self._interaction, "finishing_type", None)
+            ),
+        )
+        return super().release()
+
 
 class ShadySimDealsRabbitHoleInteraction(SuperInteraction):
     def on_added_to_queue(self, *args, **kwargs):
-        liability = HideSimLiability(self)
+        liability = _LoggedHideSimLiability(self)
         self.add_liability(liability.LIABILITY_TOKEN, liability)
         return super().on_added_to_queue(*args, **kwargs)
--- tests/test_rabbit_hole_interaction.py
+++ tests/test_rabbit_hole_interaction.py
@@ -19,6 +19,13 @@ def test_queue_adds_native_hide_liability_before_native_handling(monkeypatch):
 
         def __init__(self, interaction):
             self.interaction = interaction
+            self._interaction = interaction
+
+        def on_run(self):
+            events.append(("native_on_run",))
+
+        def release(self):
+            events.append(("native_release",))
 
     interactions = ModuleType("interactions")
     interactions.__path__ = []
@@ -43,3 +50,25 @@ def test_queue_adds_native_hide_liability_before_native_handling(monkeypatch):
     assert events[0][:2] == ("liability", "rabbit_hole")
     assert events[0][2].interaction is interaction
     assert events[1] == ("super", (7,), {"notify_client": False})
+
+    class FakeLogger:
+        def log(self, event, **fields):
+            events.append((event, fields))
+
+    module.LOGGER = FakeLogger()
+    interaction.is_finishing_naturally = False
+    interaction.finishing_type = "TRANSITION_FAILURE"
+    liability = events[0][2]
+    liability.on_run()
+    liability.release()
+
+    assert events[2] == ("rabbit_hole_interaction_running", {})
+    assert events[3] == ("native_on_run",)
+    assert events[4] == (
+        "rabbit_hole_interaction_releasing",
+        {
+            "finishing_naturally": False,
+            "finishing_type": "TRANSITION_FAILURE",
+        },
+    )
+    assert events[5] == ("native_release",)
